749a3ca3927290e3f78d3d5ff7996165cd5b4e28
plsck <plscks@icloud.com>
2020-11-10 11:35:09 -0600

    Updates some missed things

--- script.js
+++ script.js
@@ -763,8 +763,8 @@ function keyModeMoveSelector(x, y, keyX, keyY) {
 	else if ((Z == 2 && X > 30) || (Z == 2 && Y > 40)) { updateTooltipKeyMode("out"); }
 	else if ((Z == 3 && X > 40) || (Z == 3 && Y > 40)) { updateTooltipKeyMode("out"); }
 	else if ((Z == 4 && X > 16) || (Z == 4 && Y > 12)) { updateTooltipKeyMode("out"); }
-	else if ((Z == 5 && X > 12) || (Z ==5 && Y > 13)) { updateTooltipKeyMode("out"); }
-	else if ((Z == 6 && X > 30) || (Z ==5 && Y > 12)) { updateTooltipKeyMode("out"); }
+	else if ((Z == 5 && X > 12) || (Z == 5 && Y > 13)) { updateTooltipKeyMode("out"); }
+	else if ((Z == 6 && (X > 30 || X < 20)) || (Z == 6 && Y > 12)) { updateTooltipKeyMode("out"); }
 	else { updateTooltipKeyMode("in"); }
 }
 
